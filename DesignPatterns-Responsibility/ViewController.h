1761c6304f7d286d9866ebe1654d3cd75f723cc3
renjinwei <renjwjx@gmail.com>
2018-05-07 06:33:59 +0800

    Initial Commit

---
+++ DesignPatterns-Responsibility/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DesignPatterns-Responsibility
+//
+//  Created by jinren on 07/05/2018.
+//  Copyright © 2018 jinren. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ DesignPatterns-Responsibility/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  DesignPatterns-Responsibility
+//
+//  Created by jinren on 07/05/2018.
+//  Copyright © 2018 jinren. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
